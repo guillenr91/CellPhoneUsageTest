8bfa24101cb50a18e347d63c44968abdc0a5831b
Randy Guillen <guillenr91@gmail.com>
2021-10-18 15:59:50 -0600

    - Application basic structure

---
+++ src/main/java/org/rgr/test/cellphoneusagetest/CellPhoneUsageTestApplication.java
@@ -0,0 +1,13 @@
+package org.rgr.test.cellphoneusagetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CellPhoneUsageTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CellPhoneUsageTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rgr/test/cellphoneusagetest/CellPhoneUsageTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rgr.test.cellphoneusagetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CellPhoneUsageTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
